ad64891410b1bcb2c839279378ba1024019e6c88
akinayturan <akinayturan@gmail.com>
2020-06-22 10:59:41 +0300

    edt 2

--- src/app/common/FileDownload.js
+++ src/app/common/FileDownload.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-import { Badge, Card, FormGroup, Input, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
-
-import prettysize from 'prettysize';
 
 export default class FileDownload extends Component {
   render() {
-    return this.props.value && <div className={this.props.className}>
+    return this.props.value && <div className={this.props.className + " my-1"} >
       <a href={"https://api.iseser.com/uploads/dosya/" + this.props.value.f_klasorismi + "/" + this.props.value.f_dosyaisimi} target="_blank" className="btn btn-sm btn-outline-primary"><i className="fa fa-download" /> {this.props.label}</a>
     </div>;
   }
--- src/app/pages/Submission/List.js
+++ src/app/pages/Submission/List.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
-import {Card, CardBody} from 'reactstrap';
+import {Card, CardBody,  FormGroup} from 'reactstrap';
 import GenelList from '../../common/GenelList';
-
-import {FormattedMessage} from "react-intl";
 import FileDownload from '../../common/FileDownload';
- 
+
 export default class List extends Component {
     render() {
         return <div className="animated fadeIn">
@@ -30,18 +28,27 @@ export default class List extends Component {
                                 <td>{row.parampap && row.parampap.value}</td>
                                 <td>{row.parampre && row.parampre.value}</td>
                                 <td>{row.authors.map((author, index) => <div>
-                                <div>{index + 1}. {author.name} {author.surname}
-                                    {!!author.correspond && <span>(Correspond)</span>}
-                                    {!!author.presenter && <span>(Presenter)</span>}
-                                </div>
+                                    <div>{index + 1}. {author.name} {author.surname}
+                                        {!!author.correspond && <span>(Correspond)</span>}
+                                        {!!author.presenter && <span>(Presenter)</span>}
+                                    </div>
                                 </div>)}</td>
                                 <td>
-                                    <FileDownload label="Abstract" value={row.abstract_dosya} />
-                                    <FileDownload label="Full Paper" value={row.full_paper_dosya} />
-                                    <FileDownload label="Presentation" value={row.poster_presentation_dosya} />
+                                    <div className="col-md-12">
+                                        <FileDownload label="Abstract" value={row.abstract_dosya}/>
+                                        <FileDownload label="Full Paper" value={row.full_paper_dosya}/>
+                                        <FileDownload label="Presentation" value={row.poster_presentation_dosya}/>
+                                        {row.video && <FormGroup row>
+                                            <div>
+                                                <a href={`https://drive.google.com/file/d/${row.video.g_dosyaismi}/preview`} target="_blank"
+                                                   className="btn btn-sm btn-outline-primary">
+                                                    <i className="fas fa-file-video"></i> Watch Video
+                                                </a>
+                                            </div>
+                                        </FormGroup>}</div>
                                 </td>
                                 <td>
-                                    { row.user.name } { row.user.surname } 
+                                    {row.user.name} {row.user.surname}
                                 </td>
                             </>
                         }
